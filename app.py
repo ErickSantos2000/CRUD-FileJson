dc6aa16a2230aa94d95a21e855e6b3f67e284b6a
ErickSantos2000 <devErickFelipe@gmail.com>
2026-05-02 15:05:19 -0300

    feat: implementa busca de avicultores em arquivo JSON

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
 from models.Avicultor import Avicultor
 import os, json
 
@@ -6,7 +6,22 @@ app = Flask(__name__)
 
 @app.get("/avicultores")
 def getAvicultores():
-    pass
+    # nome do arquivo JSON
+    arquivo_json = "avicultores.json"
+
+    # verifica se o arquivo JSON existe
+    if os.path.exists(arquivo_json):
+        # abre o arquivo no modo leitura "r" read
+        # with é um gerenciador de contexto, ele garante que o arquivo seja fechado assim que o codigo terminar a execução
+        # as f cria um apelido para o arquivo aberto 
+        with open(arquivo_json, "r") as f:
+            # le o arquivo JSON e converte para um OBJ Python, neste caso uma lista ou dicionario
+            lista_avicultores = json.load(f)
+    
+    else:
+        lista_avicultores = []
+
+    return lista_avicultores, 200
 
 
 @app.post("/avicultores")
@@ -44,14 +59,13 @@ def postAvicultores():
     # o modo "w" apaga tudo que existia no arquivo e escreve a lista atualizada
     with open(arquivo_json, "w") as f:
         # pega lista de diciorios e a traduz para o formato JSON
-        json.dump(lista_avicultores, f, indent=4J, ensure_ascii=False)
+        json.dump(lista_avicultores, f, indent=4, ensure_ascii=False)
 
     return {"mensagem": f"Avicultor {novo_avicultor.nome} cadastrado!"}, 201
 
 @app.put("/avicultores")
 def putAvicultores():
     pass
-
 
 @app.delete("/avicultores")
 def deleteAvicultores():
